48ffc971507d5e32ffc9779018f457b6c20be2c4
SimonTheFirst <kirillsimonov@yandex.ru>
2018-11-21 09:27:53 +0300

    its a test commit

--- tp_CourseWork/CourseWorkClient/src/courseWorkClient/common/DataBaseInterface.java
+++ tp_CourseWork/CourseWorkClient/src/courseWorkClient/common/DataBaseInterface.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataBaseInterface {
 	private static final String url = "jdbc:mysql://localhost:3306/coursework?useSSL=false&serverTimezone=Europe/Moscow";
 	private static final String user = "root";
-	private static final String password = "2hj7x0rHitman";//enter your password here
+	private static final String password = "";//enter your password here
 	static Connection connection;
 	
 	public Connection connect()
